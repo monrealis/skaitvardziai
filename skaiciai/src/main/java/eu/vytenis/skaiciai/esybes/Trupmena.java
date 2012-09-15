176b72e8604d311f2148c4abfb45c68a5d1fc34b
Vytenis Bivainis <monrealis@gmail.com>
2012-09-15 19:30:56 +0300

    Pridėta trupmena

---
+++ skaiciai/src/main/java/eu/vytenis/skaiciai/esybes/Trupmena.java
@@ -0,0 +1,20 @@
+package eu.vytenis.skaiciai.esybes;
+
+public class Trupmena {
+	private long skaitiklis;
+	private long vardiklis;
+	
+	public Trupmena(long skaitiklis, long vardiklis) {
+		this.skaitiklis = skaitiklis;
+		this.vardiklis = vardiklis;
+	}
+	
+	public long getSkaitiklis() {
+		return skaitiklis;
+	}
+	
+	public long getVardiklis() {
+		return vardiklis;
+	}
+
+}

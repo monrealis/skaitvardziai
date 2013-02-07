132088cc2485fd060c923ad45fc839a9c923ddf6
Vytenis Bivainis <monrealis@gmail.com>
2013-02-07 17:44:36 +0200

    Pridėta slf4j loginimo realizacija.

---
+++ skaitvardziai-slf4j/src/main/java/eu/vytenis/skaitvardziai/slf4j/Slf4jLog.java
@@ -0,0 +1,26 @@
+package eu.vytenis.skaitvardziai.slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eu.vytenis.skaitvardziai.log.Log;
+
+public class Slf4jLog implements Log {
+	
+	private final Logger logger;
+	
+	public Slf4jLog(Class<?> type) {
+		 logger = LoggerFactory.getLogger(type);
+	}
+	
+	public boolean isWarnEnabled() {
+		return logger.isWarnEnabled();
+	}
+	
+	public void warn(String text) {
+		logger.warn(text);
+	}
+
+
+
+}

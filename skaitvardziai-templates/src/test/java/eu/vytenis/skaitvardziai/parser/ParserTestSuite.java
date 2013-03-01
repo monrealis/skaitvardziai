18944d0a9584e88f8693a16dc2a23cceafa28aa0
Vytenis Bivainis <monrealis@gmail.com>
2013-03-01 22:38:10 +0200

    Pridėtas ParserTestSuite.

---
+++ skaitvardziai-templates/src/test/java/eu/vytenis/skaitvardziai/parser/ParserTestSuite.java
@@ -0,0 +1,11 @@
+package eu.vytenis.skaitvardziai.parser;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuiteClasses({NodesTest.class, SkaitvardziaiFunctionParserTest.class})
+@RunWith(Suite.class)
+public class ParserTestSuite {
+
+}

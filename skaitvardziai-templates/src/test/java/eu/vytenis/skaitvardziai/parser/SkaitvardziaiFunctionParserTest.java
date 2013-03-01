98c1975468b5f5f9a20eae5dc70751d39c60bcc1
Vytenis Bivainis <monrealis@gmail.com>
2013-03-01 16:07:34 +0200

    Rašomas parseris.

--- skaitvardziai-templates/src/test/java/eu/vytenis/skaitvardziai/parser/SkaitvardziaiFunctionParserTest.java
+++ skaitvardziai-templates/src/test/java/eu/vytenis/skaitvardziai/parser/SkaitvardziaiFunctionParserTest.java
@@ -1,6 +1,8 @@
 package eu.vytenis.skaitvardziai.parser;
 
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,28 +10,71 @@ import org.junit.Test;
 import eu.vytenis.parser.ParseException;
 import eu.vytenis.parser.SimpleNode;
 import eu.vytenis.parser.SkaitvardziaiFunctionParser;
+import eu.vytenis.parser.SkaitvardziaiFunctionParserTreeConstants;
 
+
+// TODO sutvarkyti, iškelti į src/main
 public class SkaitvardziaiFunctionParserTest {
 	
+	private List<SimpleNode> getChildren(SimpleNode node, int id) {
+		List<SimpleNode> r = new ArrayList<SimpleNode>();
+		for (int i = 0; i < node.jjtGetNumChildren(); ++i) {
+			SimpleNode n = (SimpleNode) node.jjtGetChild(i);
+			if (n.toString().equals(SkaitvardziaiFunctionParserTreeConstants.jjtNodeName[id])) {
+				r.add(n);
+			}
+		}
+		return r;		
+	}
+	
+	private SimpleNode getChild(SimpleNode node, int id) {
+		List<SimpleNode> r = getChildren(node, id);
+		return r != null ? r.iterator().next() : null;
+	}
+	
+	private Object[] getArgs(SimpleNode call) {
+		SimpleNode argsNode = getChild(call, SkaitvardziaiFunctionParserTreeConstants.JJTARGUMENTS);
+		List<SimpleNode> argNodes = getChildren(argsNode, SkaitvardziaiFunctionParserTreeConstants.JJTARGUMENT);
+		
+		List<Object> r = new ArrayList<Object>();
+		for (SimpleNode n : argNodes) {
+			r.add(n);
+		}
+		
+		return r.toArray(new Object[] {});
+	}
+	
+	
+	
 	@Test
-	public void testStart() throws ParseException {
+	public void testFunctionCalls() throws ParseException {
 		SimpleNode n;
-		n = parse("ff(1)");
-		n = parse(" ff(1)");
-		n = parse("ff(1) ");
-		n = parse(" ff(1) ");
-		n.dump("");
-		n = parse("ff(1, 1)");
-		n = parse("ff('a')");
-		n = parse("ff(\"b\")");
-		n.dump("");
-		Assert.assertNotNull(n);
+		n = parse("ff(1)", 1);
+		n = parse(" ff(1)", 1);
+		n = parse("ff(1) ", 1);
+		n = parse(" ff(1) ", 1);;
+		n = parse("ff(1, 1)", 2);
+		
+		n = parse("ff( 'a')", 1);
+		n = parse("ff(\"b\" )", 1);
+		
+		n = parse("ff( 2 / 3 )", 1);
+		
+		n = n != null ? n : null;
+		
 		
 		
 	}
 
-	private SimpleNode parse(String text) throws ParseException {
-		return new SkaitvardziaiFunctionParser(new StringReader(text)).start();
+	private SimpleNode parse(String text, int argsCount) throws ParseException {
+		SimpleNode call = new SkaitvardziaiFunctionParser(new StringReader(text)).FunctionCall();
+		System.out.println();
+		System.out.println(text);
+		call.dump("");
+		Object[] args = getArgs(call);
+		Assert.assertEquals(argsCount, args.length);
+		Assert.assertNotNull(text);
+		return call;		
 	}
 
 

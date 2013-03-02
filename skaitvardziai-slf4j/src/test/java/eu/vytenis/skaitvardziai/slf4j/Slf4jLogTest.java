e12aa4864c17c36665aef06f59a9a4428489e0f4
Vytenis Bivainis <monrealis@gmail.com>
2013-03-02 14:00:59 +0200

    Slf4j pridėtas testas.

---
+++ skaitvardziai-slf4j/src/test/java/eu/vytenis/skaitvardziai/slf4j/Slf4jLogTest.java
@@ -0,0 +1,12 @@
+package eu.vytenis.skaitvardziai.slf4j;
+
+import org.junit.Test;
+
+public class Slf4jLogTest {
+
+	@Test
+	public void testCreateSucceeds() {
+		new Slf4jLog(Slf4jLogTest.class);
+		
+	}
+}

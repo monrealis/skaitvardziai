902565633976b506ce34e4f3f728fe10f6788844
Vytenis Bivainis <monrealis@gmail.com>
2013-03-06 20:49:02 +0200

    Didelių kelintinių testai.

--- skaitvardziai-core/src/test/java/eu/vytenis/skaitvardziai/skaiciai/KelintiniaiTest.java
+++ skaitvardziai-core/src/test/java/eu/vytenis/skaitvardziai/skaiciai/KelintiniaiTest.java
@@ -1,6 +1,7 @@
 package eu.vytenis.skaitvardziai.skaiciai;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -52,43 +53,6 @@ public abstract class KelintiniaiTest extends SkaitvardziaiTest {
 		super.testSkaiciai(skaiciai, f);
 	}
 	
-	public void testVirst1000VBigIntegers(Gimine gimine) {
-		boolean vyrg = gimine == Gimine.V;
-		BigInteger e18 = Numbers.BILLION.multiply(Numbers.BILLION);
-		BigInteger e9 = Numbers.BILLION;
-		BigInteger e6 = Numbers.MILLION;
-//		BigInteger e3 = Numbers.THOUSAND;
-		
-		Map<BigInteger, String> bigNumbers = new TreeMap<BigInteger, String>();
-		Map<BigInteger, String> smallNumbers = new TreeMap<BigInteger, String>();
-		
-		
-		bigNumbers.put(e9, vyrg ? "milijardasis" : "milijardoji");		
-		
-//		bigNumbers.put(BigInteger.valueOf(  1).multiply(e3).multiply(e9), "tūkstantis milijardų");
-//		bigNumbers.put(BigInteger.valueOf(  5).multiply(e3).multiply(e9), "penki tūkstančiai milijardų");
-//		bigNumbers.put(BigInteger.valueOf( 50).multiply(e3).multiply(e9), "penkiasdešimt tūkstančių milijardų");
-//		bigNumbers.put(BigInteger.valueOf(732).multiply(e3).multiply(e9), "septyni šimtai trisdešimt du tūkstančiai milijardų");
-//		
-		bigNumbers.put(e18, vyrg ? "milijardas milijardasis" : "milijardas milijardoji");		
-//		bigNumbers.put(BigInteger.valueOf(  1).multiply(e6).multiply(e18), "milijonas milijardų milijardų");
-//		bigNumbers.put(BigInteger.valueOf(  2).multiply(e6).multiply(e18), "du milijonai milijardų milijardų");
-//		bigNumbers.put(BigInteger.valueOf( 12).multiply(e6).multiply(e18), "dvylika milijonų milijardų milijardų");
-//		bigNumbers.put(BigInteger.valueOf(204).multiply(e3).multiply(e9), "du šimtai keturi tūkstančiai milijardų");
-		
-		smallNumbers.put(e6, vyrg ? "milijonasis" : "milijonoji");
-//		String prefix = "šeši šimtai penkiasdešimt du milijonai aštuoni šimtai septyniasdešimt keturi tūkstančiai du šimtai ";
-//		smallNumbers.put(new BigInteger("652874203"), prefix + "trys");
-//		smallNumbers.put(new BigInteger("652874201"), prefix + (gimine == Gimine.V ? "vienas" : "viena"));
-//		smallNumbers.put(new BigInteger("652874250"), prefix + "penkiasdešimt");
-		
-		//Map<BigInteger, String> combinedNumbers = createCombinedNumbers(bigNumbers, smallNumbers);
-		
-		testSkaiciai(bigNumbers, Skaicius.V, Linksnis.V, gimine);
-		testSkaiciai(smallNumbers, Skaicius.V, Linksnis.V, gimine);
-		//testSkaiciai(combinedNumbers, Skaicius.V, Linksnis.V, gimine);
-	}
-	
 	@Test
 	public void testVirst1000VVyrBigIntegers() {
 		testVirst1000VBigIntegers(Gimine.V);
@@ -97,6 +61,69 @@ public abstract class KelintiniaiTest extends SkaitvardziaiTest {
 	@Test
 	public void testVirst1000VMotBigIntegers() {
 		testVirst1000VBigIntegers(Gimine.M);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void testVirst1000VBigIntegers(Gimine gimine) {
+		BigInteger e18 = Numbers.BILLION.multiply(Numbers.BILLION);
+		BigInteger e9 = Numbers.BILLION;
+		BigInteger e6 = Numbers.MILLION;
+		BigInteger e3 = Numbers.THOUSAND;
+		
+		Map<BigInteger, String> bigNumbers = new TreeMap<BigInteger, String>();
+		Map<BigInteger, String> smallNumbers = new TreeMap<BigInteger, String>();
+		
+		bigNumbers.put(e9, "milijardasis");		
+		
+		bigNumbers.put(BigInteger.valueOf(  1).multiply(e3).multiply(e9), "tūkstantis milijardasis");
+		bigNumbers.put(BigInteger.valueOf(  5).multiply(e3).multiply(e9), "penki tūkstančiai milijardasis");
+		bigNumbers.put(BigInteger.valueOf( 50).multiply(e3).multiply(e9), "penkiasdešimt tūkstančių milijardasis");
+		bigNumbers.put(BigInteger.valueOf(732).multiply(e3).multiply(e9), "septyni šimtai trisdešimt du tūkstančiai milijardasis");
+//		
+		bigNumbers.put(e18, "milijardas milijardasis");		
+		bigNumbers.put(BigInteger.valueOf(  1).multiply(e6).multiply(e18), "milijonas milijardų milijardasis");
+		bigNumbers.put(BigInteger.valueOf(  2).multiply(e6).multiply(e18), "du milijonai milijardų milijardasis");
+		bigNumbers.put(BigInteger.valueOf( 12).multiply(e6).multiply(e18), "dvylika milijonų milijardų milijardasis");
+		bigNumbers.put(BigInteger.valueOf(204).multiply(e3).multiply(e9), "du šimtai keturi tūkstančiai milijardasis");
+		
+		smallNumbers.put(e6, "milijonasis");
+		String prefix = "šeši šimtai penkiasdešimt du milijonai aštuoni šimtai septyniasdešimt keturi tūkstančiai du šimtai ";
+		smallNumbers.put(new BigInteger("652874203"), prefix + "trečiasis");
+		smallNumbers.put(new BigInteger("652874201"), prefix + "pirmasis");
+		smallNumbers.put(new BigInteger("652874250"), prefix + "penkiasdešimtasis");
+		
+		Map<BigInteger, String> combinedNumbers = new HashMap<BigInteger, String>();
+		combinedNumbers.put(BigInteger.valueOf(204).multiply(e3).multiply(e9).add(new BigInteger("652874250")), "du šimtai keturi tūkstančiai milijardų " + prefix + "penkiasdešimtasis");
+			
+		for (Map<BigInteger, String> map: Arrays.<Map<BigInteger, String>>asList(bigNumbers, smallNumbers, combinedNumbers)) {
+			if (gimine == Gimine.M) {
+				replaceInValues(map, "milijardasis", "milijardoji");
+				replaceInValues(map, "milijonasis", "milijonoji");
+				replaceInValues(map, "trečiasis", "trečioji");
+				replaceInValues(map, "pirmasis", "pirmoji");
+				replaceInValues(map, "penkiasdešimtasis", "penkiasdešimtoji");
+			}
+			if (rusis == Rusis.P) {
+				replaceInValues(map, "trečiasis", "trečias");
+				replaceInValues(map, "pirmasis", "pirmas");
+				replaceInValues(map, "penkiasdešimtasis", "penkiasdešimtas");
+				replaceInValues(map, "trečioji", "trečia");
+				replaceInValues(map, "pirmoji", "pirma");
+				replaceInValues(map, "penkiasdešimtoji", "penkiasdešimta");
+			}
+		}
+		
+		testSkaiciai(bigNumbers, Skaicius.V, Linksnis.V, gimine);
+		testSkaiciai(smallNumbers, Skaicius.V, Linksnis.V, gimine);
+		testSkaiciai(combinedNumbers, Skaicius.V, Linksnis.V, gimine);
+	}
+	
+	private void replaceInValues(Map<?, String> map, String regex, String replacement) {
+		for (Entry<?, String> e : map.entrySet()) {
+			String v = e.getValue();
+			v = v.replaceAll(regex, replacement);
+			e.setValue(v);
+		}
 	}
 
 }

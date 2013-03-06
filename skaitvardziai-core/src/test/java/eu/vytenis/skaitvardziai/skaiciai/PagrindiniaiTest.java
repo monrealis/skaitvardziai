8164427f2cc647b75e21a0bc921950d68d4d26d8
Vytenis Bivainis <monrealis@gmail.com>
2013-03-06 19:08:10 +0200

    Parašyti didelių skaičių pagrindinių skaitvardžių kilmininko ir naudininko testai.

--- skaitvardziai-core/src/test/java/eu/vytenis/skaitvardziai/skaiciai/PagrindiniaiTest.java
+++ skaitvardziai-core/src/test/java/eu/vytenis/skaitvardziai/skaiciai/PagrindiniaiTest.java
@@ -69,4 +69,80 @@ public abstract class PagrindiniaiTest extends SkaitvardziaiTest {
 		testSkaiciai(smallNumbers, Linksnis.V);
 		testSkaiciai(combinedNumbers, Linksnis.V);
 	}
+	
+	@Test
+	public void testVirst1000KBigIntegers() {
+		Checks.checkNotNull("gimine", gimine);
+		
+		BigInteger e18 = Numbers.BILLION.multiply(Numbers.BILLION);
+		BigInteger e9 = Numbers.BILLION;
+		BigInteger e6 = Numbers.MILLION;
+		BigInteger e3 = Numbers.THOUSAND;
+		
+		Map<BigInteger, String> bigNumbers = new TreeMap<BigInteger, String>();
+		Map<BigInteger, String> smallNumbers = new TreeMap<BigInteger, String>();
+		
+		bigNumbers.put(e9, "milijardo");		
+		
+		bigNumbers.put(BigInteger.valueOf(  1).multiply(e3).multiply(e9), "tūkstančio milijardų");
+		bigNumbers.put(BigInteger.valueOf(  5).multiply(e3).multiply(e9), "penkių tūkstančių milijardų");
+		bigNumbers.put(BigInteger.valueOf( 50).multiply(e3).multiply(e9), "penkiasdešimties tūkstančių milijardų");
+		bigNumbers.put(BigInteger.valueOf(732).multiply(e3).multiply(e9), "septynių šimtų trisdešimt dviejų tūkstančių milijardų");
+		
+		bigNumbers.put(e18, "milijardo milijardų");		
+		bigNumbers.put(BigInteger.valueOf(  1).multiply(e6).multiply(e18), "milijono milijardų milijardų");
+		bigNumbers.put(BigInteger.valueOf(  2).multiply(e6).multiply(e18), "dviejų milijonų milijardų milijardų");
+		bigNumbers.put(BigInteger.valueOf( 12).multiply(e6).multiply(e18), "dvylikos milijonų milijardų milijardų");
+		bigNumbers.put(BigInteger.valueOf(204).multiply(e3).multiply(e9), "dviejų šimtų keturių tūkstančių milijardų");
+		
+		smallNumbers.put(e6, "milijono");
+		String prefix = "šešių šimtų penkiasdešimt dviejų milijonų aštuonių šimtų septyniasdešimt keturių tūkstančių dviejų šimtų ";
+		smallNumbers.put(new BigInteger("652874203"), prefix + "trijų");
+		smallNumbers.put(new BigInteger("652874201"), prefix + (gimine == Gimine.V ? "vieno" : "vienos"));
+		smallNumbers.put(new BigInteger("652874250"), prefix + "penkiasdešimties");
+		
+		Map<BigInteger, String> combinedNumbers = createCombinedNumbers(bigNumbers, smallNumbers);
+		
+		testSkaiciai(bigNumbers, Linksnis.K);
+		testSkaiciai(smallNumbers, Linksnis.K);
+		testSkaiciai(combinedNumbers, Linksnis.K);
+	}
+	
+	@Test
+	public void testVirst1000NBigIntegers() {
+		Checks.checkNotNull("gimine", gimine);
+		
+		BigInteger e18 = Numbers.BILLION.multiply(Numbers.BILLION);
+		BigInteger e9 = Numbers.BILLION;
+		BigInteger e6 = Numbers.MILLION;
+		BigInteger e3 = Numbers.THOUSAND;
+		
+		Map<BigInteger, String> bigNumbers = new TreeMap<BigInteger, String>();
+		Map<BigInteger, String> smallNumbers = new TreeMap<BigInteger, String>();
+		
+		bigNumbers.put(e9, "milijardui");		
+		
+		bigNumbers.put(BigInteger.valueOf(  1).multiply(e3).multiply(e9), "tūkstančiui milijardų");
+		bigNumbers.put(BigInteger.valueOf(  5).multiply(e3).multiply(e9), "penkiems tūkstančiams milijardų");
+		bigNumbers.put(BigInteger.valueOf( 50).multiply(e3).multiply(e9), "penkiasdešimčiai tūkstančių milijardų");
+		bigNumbers.put(BigInteger.valueOf(732).multiply(e3).multiply(e9), "septyniems šimtams trisdešimt dviem tūkstančiams milijardų");
+		
+		bigNumbers.put(e18, "milijardui milijardų");		
+		bigNumbers.put(BigInteger.valueOf(  1).multiply(e6).multiply(e18), "milijonui milijardų milijardų");
+		bigNumbers.put(BigInteger.valueOf(  2).multiply(e6).multiply(e18), "dviem milijonams milijardų milijardų");
+		bigNumbers.put(BigInteger.valueOf( 12).multiply(e6).multiply(e18), "dvylikai milijonų milijardų milijardų");
+		bigNumbers.put(BigInteger.valueOf(204).multiply(e3).multiply(e9), "dviem šimtams keturiems tūkstančiams milijardų");
+		
+		smallNumbers.put(e6, "milijonui");
+		String prefix = "šešiems šimtams penkiasdešimt dviem milijonams aštuoniems šimtams septyniasdešimt keturiems tūkstančiams dviem šimtams ";
+		smallNumbers.put(new BigInteger("652874203"), prefix + "trims");
+		smallNumbers.put(new BigInteger("652874201"), prefix + (gimine == Gimine.V ? "vienam" : "vienai"));
+		smallNumbers.put(new BigInteger("652874250"), prefix + "penkiasdešimčiai");
+		
+		Map<BigInteger, String> combinedNumbers = createCombinedNumbers(bigNumbers, smallNumbers);
+		
+		testSkaiciai(bigNumbers, Linksnis.N);
+		testSkaiciai(smallNumbers, Linksnis.N);
+		testSkaiciai(combinedNumbers, Linksnis.N);
+	}
 }

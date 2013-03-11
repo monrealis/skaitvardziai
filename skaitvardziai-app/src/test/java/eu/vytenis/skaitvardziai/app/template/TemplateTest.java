e599f0dbc86503ed8b92ba53776a4ad615dc80e9
Vytenis Bivainis <monrealis@gmail.com>
2013-03-11 23:14:58 +0200

    Pridėtas kompleksinio šablono pavyzdys testuose.

--- skaitvardziai-app/src/test/java/eu/vytenis/skaitvardziai/app/template/TemplateTest.java
+++ skaitvardziai-app/src/test/java/eu/vytenis/skaitvardziai/app/template/TemplateTest.java
@@ -11,12 +11,12 @@ import eu.vytenis.skaitvardziai.parser.methods.Methods.SkaitvardziaiParseExcepti
 public class TemplateTest extends AppTest {
 
 	@Test
-	public void testStatic() {
+	public void staticTextRemainsUnchanged() {
 		assertOutByIn(new ExpectedOut("test\na"), "test\na", "-t");
 	}
 	
 	@Test
-	public void testSveikieji() {
+	public void sveikiejiReplacementSucceeds() {
 		assertOutByIn(new ExpectedOut("Skaičius: vienas"), "Skaičius: ${sveikasis(1)}", "-t");
 		assertOutByIn(new ExpectedOut("Skaičius: trys šimtai tūkstančių keturi šimtai penkiasdešimt"), "Skaičius: ${sveikasis(300450)}", "-t");		
 		assertOutByIn(new ExpectedOut("Skaičius: penki šimtai dvyliktąsias"), "Skaičius: ${sveikasis(512, 'Iv,Kl,SD,G,MG')}", "-t");		
@@ -24,13 +24,13 @@ public class TemplateTest extends AppTest {
 	}
 	
 	@Test
-	public void testTrupmenos() {
+	public void trupmenosReplacementSucceeds() {
 		assertOutByIn(new ExpectedOut("viena antroji yra mažiau nei dvi trečiosios"), "${trupmena(1/2)} yra mažiau nei ${trupmena(2/3)}", "-t");
 		assertOutByIn(new ExpectedOut("minus keturias šeštąsias"), "${trupmena(-4/6, 'G')}", "-t");
 	}
 	
 	@Test
-	public void testKiti() {
+	public void otherWordsReplacementSucceeds() {
 		assertOutByIn(new ExpectedOut("10 žmonių"), "10 ${kiti(10, 'žmogus', 'žmonės', 'žmonių')}", "-t");
 		assertOutByIn(new ExpectedOut("1 žmogus"), "1 ${kiti(1, 'žmogus', 'žmonės', 'žmonių')}", "-t");
 		assertOutByIn(new ExpectedOut("22 žmonės"), "22 ${kiti(22, 'žmogus', 'žmonės', 'žmonių')}", "-t");
@@ -56,14 +56,14 @@ public class TemplateTest extends AppTest {
 	}
 	
 	@Test
-	public void testStartEndTags() {
+	public void replacementWithVariousStartAndEndTagsSucceeds() {
 		assertOutByIn(new ExpectedOut("Skaičius: ${sveikasis(1)}"), "Skaičius: ${sveikasis(1)}", "-t", "-s", "#[", "-e", "]");
 		assertOutByIn(new ExpectedOut("Skaičius: vienas"), "Skaičius: #[sveikasis(1)]", "-t", "-s", "#[", "-e", "]");
 		assertOutByIn(new ExpectedOut("Skaičius: vienas"), "Skaičius: <? sveikasis(1) ?>", "-t", "-s", "<?", "-e", "?>");
 	}
 	
 	@Test
-	public void testOutputEncoding() {
+	public void changingOutputEncodingSucceeds() {
 		systemIo.setOutputCharsetName(WIN1257);
 		assertOutByIn(new ExpectedOut("dešimt", WIN1257), "${sveikasis(10)}", "-t", "-oe", WIN1257);
 		systemIo.setOutputCharsetName(UTF8);
@@ -71,11 +71,22 @@ public class TemplateTest extends AppTest {
 	}
 	
 	@Test
-	public void testInputEncoding() {
+	public void changingInputEncodingSucceeds() {
 		systemIo.setInputCharsetName(WIN1257);
 		assertOutByIn(new ExpectedOut("dešimt"), "dešimt", "-t", "-ie", WIN1257);
 		systemIo.setInputCharsetName(UTF8);
 		assertOutByIn(new ExpectedOut("dešimt"), "dešimt", "-t", "-ie", UTF8);
+	}
+	
+	@Test
+	public void exampleTemplateWithVariousFunctionsSucceeds() {
+		String template = "Vidutinis darbo užmokestis (2012 III ketv.) buvo ${sveikasis(2171)} ${kiti(2171, 'litas', 'litai', 'litų')}.\n"
+				+ "Minimalioji mėnesinė alga (2013 I ketv.) buvo ${sveikasis(1000)} ${kiti(1000, 'litas', 'litai', 'litų')}.\n"
+				+ "Bazinė mėnesio alga yra ${sveikasis(122)} ${kiti(122, 'litas', 'litai', 'litų')}.\n";
+		String expected = "Vidutinis darbo užmokestis (2012 III ketv.) buvo du tūkstančiai šimtas septyniasdešimt vienas litas.\n"
+				+ "Minimalioji mėnesinė alga (2013 I ketv.) buvo tūkstantis litų.\n"
+				+ "Bazinė mėnesio alga yra šimtas dvidešimt du litai.\n"; 
+		assertOutByIn(new ExpectedOut(expected), template, "-t");
 	}
 	
 }

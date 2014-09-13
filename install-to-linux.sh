a29e7c57f6f0e9aefb7f34ca6ff1c155263678b9
Vytenis Bivainis <monrealis@gmail.com>
2014-09-14 00:53:05 +0300

    Instaliavimo skriptas iš .sh pakeistas į Makefile.

--- install-to-linux.sh
+++ install-to-linux.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
-
-cd skaitvardziai-parent
-mvn clean install
 cd ../skaitvardziai-app
-./install-gcj-exec.sh
+make install
--- skaitvardziai-app/make-gcj-exec.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-mvn package
-
-if [ ! -x target/skaiciai ] ; then
-	echo Executing gcj
-	gcj --main=eu.vytenis.skaitvardziai.app.main.Main -o target/skaiciai target/skaitvardziai-*-jar-with-dependencies.jar
-fi

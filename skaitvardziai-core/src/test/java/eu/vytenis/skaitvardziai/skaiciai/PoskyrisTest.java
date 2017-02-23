26a12b4f65e6417532407c9e60570e55b3892680
Vytenis Bivainis <monrealis@gmail.com>
2017-02-23 23:08:45 +0200

    Refactor

--- skaitvardziai-core/src/test/java/eu/vytenis/skaitvardziai/skaiciai/PoskyrisTest.java
+++ skaitvardziai-core/src/test/java/eu/vytenis/skaitvardziai/skaiciai/PoskyrisTest.java
@@ -1,32 +1,50 @@
 package eu.vytenis.skaitvardziai.skaiciai;
 
+import static eu.vytenis.skaitvardziai.klasifikatoriai.Poskyris.Dauginis;
+import static eu.vytenis.skaitvardziai.klasifikatoriai.Poskyris.Kuopinis;
+import static eu.vytenis.skaitvardziai.klasifikatoriai.Poskyris.Pagrindinis;
+import static eu.vytenis.skaitvardziai.klasifikatoriai.Poskyris.Trupmeninis;
+import static eu.vytenis.skaitvardziai.klasifikatoriai.Skyrius.Kiekinis;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import eu.vytenis.skaitvardziai.klasifikatoriai.Poskyris;
 import eu.vytenis.skaitvardziai.klasifikatoriai.Skyrius;
 
 public class PoskyrisTest {
+	private Map<Skyrius, Set<Poskyris>> poskyriai = new HashMap<Skyrius, Set<Poskyris>>();
+
+	@Before
+	public void before() {
+		prepareEmptySets();
+	}
+
+	private void prepareEmptySets() {
+		for (Skyrius skyrius : Skyrius.values())
+			poskyriai.put(skyrius, new HashSet<Poskyris>());
+	}
+
 	@Test
-	public void testSkyriuPoskyriai() {
-		Set<Poskyris> kiekiniai = new HashSet<Poskyris>();
-		Set<Poskyris> kelintiniai = new HashSet<Poskyris>();
-		for (Poskyris poskyris : Poskyris.values()) {
-			if (poskyris.getSkyrius() == Skyrius.Kelintinis) {
-				kelintiniai.add(poskyris);
-			} else if (poskyris.getSkyrius() == Skyrius.Kiekinis) {
-				kiekiniai.add(poskyris);
-			} else {
-				throw new IllegalStateException();
-			}
-		}
-		assertEquals(new HashSet<Skyrius>(asList(Skyrius.Kiekinis, Skyrius.Kelintinis)), new HashSet<Skyrius>(asList(Skyrius.values())));
-		assertEquals(new HashSet<Poskyris>(asList(Poskyris.Pagrindinis, Poskyris.Dauginis, Poskyris.Kuopinis, Poskyris.Trupmeninis)), kiekiniai);
-		assertEquals(new HashSet<Poskyris>(asList(Poskyris.Kelintinis)), kelintiniai);
+	public void skyriuPoskyriaiAsExpected() {
+		fillPoskyriai();
+		assertEquals(set(Pagrindinis, Dauginis, Kuopinis, Trupmeninis), poskyriai.get(Kiekinis));
+		assertEquals(set(Poskyris.Kelintinis), poskyriai.get(Skyrius.Kelintinis));
+	}
+
+	private void fillPoskyriai() {
+		for (Poskyris poskyris : Poskyris.values())
+			poskyriai.get(poskyris.getSkyrius()).add(poskyris);
+	}
+
+	private Set<Poskyris> set(Poskyris... poskyriai) {
+		return new HashSet<Poskyris>(asList(poskyriai));
 	}
 }

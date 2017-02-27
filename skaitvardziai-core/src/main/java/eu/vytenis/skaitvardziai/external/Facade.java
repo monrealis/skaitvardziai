d2f51c4816f933a4cf4b73e11b67a105c26f500e
Vytenis Bivainis <monrealis@gmail.com>
2017-02-27 23:02:55 +0200

    Cleanup a little bit

--- skaitvardziai-core/src/main/java/eu/vytenis/skaitvardziai/external/Facade.java
+++ skaitvardziai-core/src/main/java/eu/vytenis/skaitvardziai/external/Facade.java
@@ -1,6 +1,8 @@
 package eu.vytenis.skaitvardziai.external;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import eu.vytenis.skaitvardziai.klasifikatoriai.Forma;
 import eu.vytenis.skaitvardziai.klasifikatoriai.FormosElementas;
@@ -15,62 +17,57 @@ import eu.vytenis.skaitvardziai.util.SkaitvardziaiTextParser;
  * Metodai, kuriuos kviečia išoriniai (ne JAVA) API.
  */
 public class Facade {
-	public static SkaiciusIrLinksnis DGS_K = new SkaiciusIrLinksnis(Skaicius.D, Linksnis.K);
+	private static SkaiciusIrLinksnis DGS_K = new SkaiciusIrLinksnis(Skaicius.D, Linksnis.K);
 
 	public static String sveikasis(SveikasisSkaicius skaicius) {
 		return sveikasis(skaicius, null);
 	}
 
 	public static String sveikasis(SveikasisSkaicius skaicius, String forma) {
-		forma = getStringOrEmptyIfNull(forma);
-		Forma f = SkaitvardziaiTextParser.get().parseForma(forma, null);
+		Forma f = SkaitvardziaiTextParser.get().parseForma(nullToEmpty(forma), null);
 		return skaicius.toString(f);
 	}
 
 	public static String trupmena(Trupmena trupmena) {
-		return trupmena(trupmena, null);
+		return trupmena(trupmena, "");
 	}
 
-	@SuppressWarnings("unchecked")
 	public static String trupmena(Trupmena trupmena, String forma) {
-		forma = getStringOrEmptyIfNull(forma);
-		Forma f = SkaitvardziaiTextParser.get().parseForma(forma, Arrays.<Class<? extends FormosElementas>> asList(Linksnis.class));
+		@SuppressWarnings("unchecked")
+		List<Class<? extends FormosElementas>> supportedParmeters = Arrays.<Class<? extends FormosElementas>> asList(Linksnis.class);
+		Forma f = SkaitvardziaiTextParser.get().parseForma(nullToEmpty(forma), supportedParmeters);
 		return trupmena.toString(f.getLinksnis());
 	}
 
 	public static String kiti(SveikasisSkaicius skaicius, String vns, String dgs, String dgsKilm) {
-		return kiti(skaicius, null, vns, dgs, dgsKilm);
+		return kiti(skaicius, "", vns, dgs, dgsKilm);
 	}
 
 	public static String kiti(SveikasisSkaicius skaicius, String forma, String vns, String dgs, String dgsKilm) {
-		Forma f = SkaitvardziaiTextParser.get().parseForma(getStringOrEmptyIfNull(forma), null);
+		Forma f = SkaitvardziaiTextParser.get().parseForma(nullToEmpty(forma), null);
 		SkaiciusIrLinksnis kitas = skaicius.getKitoZodzioSkaiciusIrLinksnis(f);
-		if (kitas.equals(DGS_K)) {
+		if (kitas.equals(DGS_K))
 			return getKitasIfNextDgsK(dgs, dgsKilm, f);
-		} else if (kitas.getSkaicius() == Skaicius.D) {
+		else if (kitas.getSkaicius() == Skaicius.D)
 			return dgs;
-		} else if (kitas.getSkaicius() == Skaicius.V) {
+		else if (kitas.getSkaicius() == Skaicius.V)
 			return vns;
-		} else {
+		else
 			return null;
-		}
-	}
-
-	private static String getStringOrEmptyIfNull(String forma) {
-		if (forma == null) {
-			forma = "";
-		}
-		return forma;
 	}
 
 	private static String getKitasIfNextDgsK(String dgs, String dgsKilm, Forma forma) {
-		if (dgsKilm != null && dgsKilm.length() > 0) {
+		if (nullToEmpty(dgsKilm).length() > 0)
 			return dgsKilm;
-		} else if (/* f.getSkaicius() == Skaicius.D && */forma.getLinksnis() == Linksnis.K && dgs != null) {
+		else if (/* f.getSkaicius() == Skaicius.D && */forma.getLinksnis() == Linksnis.K && dgs != null)
 			return dgs;
-		} else {
+		else
 			return null;
-		}
 	}
 
+	private static String nullToEmpty(String forma) {
+		if (forma == null)
+			forma = "";
+		return forma;
+	}
 }

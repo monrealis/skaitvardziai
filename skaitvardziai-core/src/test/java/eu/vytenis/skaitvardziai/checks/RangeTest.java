10bfc1ee7c22d6077f85067bd9c27e1fa89fc89d
Vytenis Bivainis <monrealis@gmail.com>
2017-02-28 23:54:11 +0200

    Convert to table driven test

--- skaitvardziai-core/src/test/java/eu/vytenis/skaitvardziai/checks/RangeTest.java
+++ skaitvardziai-core/src/test/java/eu/vytenis/skaitvardziai/checks/RangeTest.java
@@ -1,15 +1,48 @@
 package eu.vytenis.skaitvardziai.checks;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class RangeTest {
-	private Range<Integer> closed = new Range<Integer>(-1, true, 1, true);
-	private Range<Integer> open = new Range<Integer>(-1, false, 1, false);
-	private Range<Integer> all = new Range<Integer>(null, false, null, false);
+	private static final Range<Integer> closed = new Range<Integer>(-1, true, 1, true);
+	private static final Range<Integer> open = new Range<Integer>(-1, false, 1, false);
+	private static final Range<Integer> all = new Range<Integer>(null, false, null, false);
+	private final Range<Integer> range;
+	private final int value;
+	private final boolean expectedContains;
+
+	@Parameters
+	public static List<Object[]> testCases() {
+		List<Object[]> cases = new ArrayList<Object[]>();
+		cases.add(createCase(closed, -2, false));
+		cases.add(createCase(closed, -1, true));
+		cases.add(createCase(closed, 0, true));
+		cases.add(createCase(closed, 1, true));
+		cases.add(createCase(closed, 2, false));
+		cases.add(createCase(open, -1, false));
+		cases.add(createCase(open, 0, true));
+		cases.add(createCase(open, 1, false));
+		cases.add(createCase(all, 0, true));
+		return cases;
+	}
+
+	public RangeTest(Range<Integer> range, int value, boolean expectedContains) {
+		this.range = range;
+		this.value = value;
+		this.expectedContains = expectedContains;
+	}
+
+	private static Object[] createCase(Range<Integer> range, int value, boolean expectedContains) {
+		return new Object[] {range, value, expectedContains};
+	}
 
 	@Test
 	public void toStringFormatsRange() {
@@ -19,24 +52,7 @@ public class RangeTest {
 	}
 
 	@Test
-	public void testSatisfiesMin() {
-		assertFalse(closed.contains(-2));
-		assertTrue(closed.contains(-1));
-		assertTrue(closed.contains(0));
-		assertFalse(open.contains(-2));
-		assertFalse(open.contains(-1));
-		assertTrue(open.contains(0));
-		assertTrue(all.contains(0));
-	}
-
-	@Test
-	public void testSatisfiesMax() {
-		assertTrue(closed.contains(0));
-		assertTrue(closed.contains(1));
-		assertFalse(closed.contains(2));
-		assertTrue(open.contains(0));
-		assertFalse(open.contains(1));
-		assertFalse(open.contains(2));
-		assertTrue(all.contains(0));
+	public void contains() {
+		assertEquals(expectedContains, range.contains(value));
 	}
 }

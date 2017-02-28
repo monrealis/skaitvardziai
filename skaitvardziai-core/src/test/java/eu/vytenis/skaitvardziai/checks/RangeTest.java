a1d6c9ae64fa217eb65189c56e86ec1729a5a766
Vytenis Bivainis <monrealis@gmail.com>
2017-02-28 23:44:29 +0200

    Refactor a little bit

--- skaitvardziai-core/src/test/java/eu/vytenis/skaitvardziai/checks/RangeTest.java
+++ skaitvardziai-core/src/test/java/eu/vytenis/skaitvardziai/checks/RangeTest.java
@@ -1,44 +1,42 @@
 package eu.vytenis.skaitvardziai.checks;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class RangeTest {
+	private Range<Integer> closed = new Range<Integer>(-1, true, 1, true);
+	private Range<Integer> open = new Range<Integer>(-1, false, 1, false);
+	private Range<Integer> all = new Range<Integer>(null, false, null, false);
 
 	@Test
-	public void testToString() {
-		assertEquals("(-infinity; infinity)", new Range<Integer>(null, false, null, false).toString());
-		assertEquals("(-8; 8)", new Range<Integer>(-8, false, 8, false).toString());
-		assertEquals("[-15; 15]", new Range<Integer>(-15, true, 15, true).toString());
+	public void toStringFormatsRange() {
+		assertEquals("(-infinity; infinity)", all.toString());
+		assertEquals("(-1; 1)", open.toString());
+		assertEquals("[-1; 1]", closed.toString());
 	}
 
 	@Test
 	public void testSatisfiesMin() {
-		Range<Integer> closed = new Range<Integer>(-1, true, 1, true);
-		assertEquals(false, closed.isSatisfiesMin(-2));
-		assertEquals(true, closed.isSatisfiesMin(-1));
-		assertEquals(true, closed.isSatisfiesMin(0));
-		Range<Integer> open = new Range<Integer>(-1, false, 1, false);
-		assertEquals(false, open.isSatisfiesMin(-2));
-		assertEquals(false, open.isSatisfiesMin(-1));
-		assertEquals(true, open.isSatisfiesMin(0));
-		Range<Integer> infinity = new Range<Integer>(null, false, null, false);
-		assertEquals(true, infinity.isSatisfiesMin(0));
+		assertFalse(closed.isSatisfiesMin(-2));
+		assertTrue(closed.isSatisfiesMin(-1));
+		assertTrue(closed.isSatisfiesMin(0));
+		assertFalse(open.isSatisfiesMin(-2));
+		assertFalse(open.isSatisfiesMin(-1));
+		assertTrue(open.isSatisfiesMin(0));
+		assertTrue(all.isSatisfiesMin(0));
 	}
 
 	@Test
 	public void testSatisfiesMax() {
-		Range<Integer> closed = new Range<Integer>(-1, true, 1, true);
-		assertEquals(true, closed.isSatisfiesMax(0));
-		assertEquals(true, closed.isSatisfiesMax(1));
-		assertEquals(false, closed.isSatisfiesMax(2));
-		Range<Integer> open = new Range<Integer>(-1, false, 1, false);
-		assertEquals(true, open.isSatisfiesMax(0));
-		assertEquals(false, open.isSatisfiesMax(1));
-		assertEquals(false, open.isSatisfiesMax(2));
-		Range<Integer> infinity = new Range<Integer>(null, false, null, false);
-		assertEquals(true, infinity.isSatisfiesMax(0));
+		assertTrue(closed.isSatisfiesMax(0));
+		assertTrue(closed.isSatisfiesMax(1));
+		assertFalse(closed.isSatisfiesMax(2));
+		assertTrue(open.isSatisfiesMax(0));
+		assertFalse(open.isSatisfiesMax(1));
+		assertFalse(open.isSatisfiesMax(2));
+		assertTrue(all.isSatisfiesMax(0));
 	}
-
 }

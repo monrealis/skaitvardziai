4cccdc39577f8bb736e9ed6ba6cadfabc2e29871
Vytenis Bivainis <monrealis@gmail.com>
2017-03-15 00:01:34 +0200

    Refactor

--- skaitvardziai-parser/src/main/java/eu/vytenis/skaitvardziai/parser/nodes/Nodes.java
+++ skaitvardziai-parser/src/main/java/eu/vytenis/skaitvardziai/parser/nodes/Nodes.java
@@ -1,38 +1,38 @@
 package eu.vytenis.skaitvardziai.parser.nodes;
 
+import static java.util.Collections.emptyList;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import eu.vytenis.skaitvardziai.exc.SkaitvardziaiRuntimeException;
 import eu.vytenis.skaitvardziai.parser.tree.SimpleNode;
 
 public class Nodes {
+	public static List<SimpleNode> getChildren(SimpleNode optionalParent, String name) {
+		if (optionalParent == null)
+			return emptyList();
+		return getChildrenByName(optionalParent, name);
+	}
 
-	public static List<SimpleNode> getChildren(SimpleNode node, String name) {
-		if (node == null) {
-			return Collections.emptyList();
-		}
-		List<SimpleNode> r = new ArrayList<SimpleNode>();
-		for (int i = 0; i < node.jjtGetNumChildren(); ++i) {
-			SimpleNode n = (SimpleNode) node.jjtGetChild(i);
-			if (n.toString().equals(name)) {
-				r.add(n);
-			}
-		}
-		return r;
+	private static List<SimpleNode> getChildrenByName(SimpleNode parent, String name) {
+		List<SimpleNode> children = new ArrayList<SimpleNode>();
+		for (int i = 0; i < parent.jjtGetNumChildren(); ++i)
+			if (parent.jjtGetChild(i).toString().equals(name))
+				children.add((SimpleNode) parent.jjtGetChild(i));
+		return children;
 	}
 
 	public static SimpleNode getOnlyChild(SimpleNode node, String name) {
 		List<SimpleNode> r = getChildren(node, name);
-		if (r.size() > 1) {
+		if (r.size() > 1)
 			throw new MoreThanOneChildException();
-		}
-		return !r.isEmpty() ? r.iterator().next() : null;
+		if (r.isEmpty())
+			return null;
+		return r.iterator().next();
 	}
 
 	public static class MoreThanOneChildException extends SkaitvardziaiRuntimeException {
-		private static final long serialVersionUID = -949095261183514187L;
+		private static final long serialVersionUID = 1;
 	}
-
 }

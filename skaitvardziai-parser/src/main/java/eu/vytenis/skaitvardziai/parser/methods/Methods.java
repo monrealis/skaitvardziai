5bb47f1793bd2cb6fe09863de29d9d5198bc20bf
Vytenis Bivainis <monrealis@gmail.com>
2017-03-15 00:09:11 +0200

    Refactor

--- skaitvardziai-parser/src/main/java/eu/vytenis/skaitvardziai/parser/methods/Methods.java
+++ skaitvardziai-parser/src/main/java/eu/vytenis/skaitvardziai/parser/methods/Methods.java
@@ -1,12 +1,16 @@
 package eu.vytenis.skaitvardziai.parser.methods;
 
+import static eu.vytenis.skaitvardziai.parser.nodes.Nodes.getChildren;
+import static eu.vytenis.skaitvardziai.parser.nodes.Nodes.getOnlyChild;
+import static eu.vytenis.skaitvardziai.parser.nodes.TreeConstants.identifier;
+import static eu.vytenis.skaitvardziai.parser.nodes.TreeConstants.parameter;
+import static eu.vytenis.skaitvardziai.parser.nodes.TreeConstants.parameters;
+
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
 import eu.vytenis.skaitvardziai.exc.SkaitvardziaiRuntimeException;
-import eu.vytenis.skaitvardziai.parser.nodes.Nodes;
-import eu.vytenis.skaitvardziai.parser.nodes.TreeConstants;
 import eu.vytenis.skaitvardziai.parser.tree.ParseException;
 import eu.vytenis.skaitvardziai.parser.tree.SimpleNode;
 import eu.vytenis.skaitvardziai.parser.tree.Token;
@@ -14,28 +18,8 @@ import eu.vytenis.skaitvardziai.parser.tree.TreeParser;
 
 public class Methods {
 	public static MethodInvocation getMethodInvocation(String methodInvocationText) {
-		SimpleNode node = parse(methodInvocationText);
-		return new MethodInvocation(getMethodName(node), getParameters(node));
-	}
-
-	private static String getMethodName(SimpleNode methodInvocationNode) {
-		SimpleNode identifier = Nodes.getOnlyChild(methodInvocationNode, TreeConstants.identifier());
-		return ((Token) identifier.jjtGetValue()).image;
-	}
-
-	private static Object[] getParameters(SimpleNode methodInvocationNode) {
-		SimpleNode node = Nodes.getOnlyChild(methodInvocationNode, TreeConstants.parameters());
-		List<SimpleNode> parameterNodes = Nodes.getChildren(node, TreeConstants.parameter());
-		List<Object> parameters = new ArrayList<Object>();
-		for (SimpleNode n : parameterNodes)
-			parameters.add(getParameter(n));
-		return parameters.toArray(new Object[] {});
-	}
-
-	private static Object getParameter(SimpleNode parameterNode) {
-		SimpleNode childNode = (SimpleNode) parameterNode.jjtGetChild(0);
-		String nodeName = childNode.toString();
-		return ParameterHandlers.byName(nodeName).getValue(childNode);
+		SimpleNode methodInvocationNode = parse(methodInvocationText);
+		return new MethodInvocation(getMethodName(methodInvocationNode), getParameters(methodInvocationNode));
 	}
 
 	static SimpleNode parse(String methodInvocationText) {
@@ -44,6 +28,26 @@ public class Methods {
 		} catch (ParseException e) {
 			throw new SkaitvardziaiParseException(e);
 		}
+	}
+
+	private static String getMethodName(SimpleNode methodInvocationNode) {
+		SimpleNode identifier = getOnlyChild(methodInvocationNode, identifier());
+		return ((Token) identifier.jjtGetValue()).image;
+	}
+
+	private static Object[] getParameters(SimpleNode methodInvocationNode) {
+		SimpleNode container = getOnlyChild(methodInvocationNode, parameters());
+		List<SimpleNode> parameterNodes = getChildren(container, parameter());
+		List<Object> parameters = new ArrayList<Object>();
+		for (SimpleNode n : parameterNodes)
+			parameters.add(getParameterValue(n));
+		return parameters.toArray(new Object[] {});
+	}
+
+	private static Object getParameterValue(SimpleNode parameterNode) {
+		SimpleNode childNode = (SimpleNode) parameterNode.jjtGetChild(0);
+		String nodeName = childNode.toString();
+		return ParameterHandlers.byName(nodeName).getValue(childNode);
 	}
 
 	public static class SkaitvardziaiParseException extends SkaitvardziaiRuntimeException {

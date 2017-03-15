83f8ef534728cb1033f6d9069a2b4500c3d92030
Vytenis Bivainis <monrealis@gmail.com>
2017-03-16 00:09:12 +0200

    Refactor

--- skaitvardziai-parser/src/test/java/eu/vytenis/skaitvardziai/parser/methods/InvokerTest.java
+++ skaitvardziai-parser/src/test/java/eu/vytenis/skaitvardziai/parser/methods/InvokerTest.java
@@ -6,59 +6,56 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class InvokerTest {
-	private Invoker parentAndChildInvoker;
-	private Invoker childInvoker;
+	private Invoker parentAndChildInvoker = new Invoker();
+	private Invoker childInvoker = new Invoker();
 
 	@Before
 	public void before() {
-		parentAndChildInvoker = new Invoker();
-		childInvoker = new Invoker();
 		parentAndChildInvoker.addPublicStaticMethods(ChildClass.class, ParentClass.class);
 		childInvoker.addPublicStaticMethods(ChildClass.class, ChildClass.class);
 	}
 
 	@Test
-	public void testAddPublicStaticMethods() {
+	public void publicStaticMethodsAdded() {
 		assertEquals(0, new Invoker().getMethods().size());
 		assertEquals(4, parentAndChildInvoker.getMethods().size());
 		assertEquals(3, childInvoker.getMethods().size());
 	}
 
 	@Test(expected = Invoker.DuplicateMethodException.class)
-	public void testAddPublicStaticMethods_Duplicate() {
+	public void duplicateMethodsNotAllowed() {
 		childInvoker.addPublicStaticMethods(ChildClass.class, ChildClass.class);
 	}
 
 	@Test
-	public void testInvoke_Success() {
+	public void invokes() {
 		assertEquals("child-f1", parentAndChildInvoker.invoke(new MethodInvocation("f1")));
 		assertEquals("f11", parentAndChildInvoker.invoke(new MethodInvocation("f11")));
 		assertEquals(25, parentAndChildInvoker.invoke(new MethodInvocation("f2", 10, 15)));
 	}
 
 	@Test
-	public void testInvoke_OverloadedMethodsSuccess() {
+	public void invokesOverloaded() {
 		assertEquals("f11", parentAndChildInvoker.invoke(new MethodInvocation("f11")));
 		assertEquals("f11-1", parentAndChildInvoker.invoke(new MethodInvocation("f11", "a")));
 	}
 
 	@Test(expected = Invoker.MethodNotFoundException.class)
-	public void testInvoke_FailedNoMethodName() {
+	public void invokeFailsIfMethodNotFound() {
 		parentAndChildInvoker.invoke(new MethodInvocation("x"));
 	}
 
 	@Test(expected = Invoker.MethodNotFoundException.class)
-	public void testInvoke_FailedNoMethodParams() {
+	public void invokeFailsBecauseOfInvalidNumberOfArguments() {
 		parentAndChildInvoker.invoke(new MethodInvocation("f2", 15));
 	}
 
 	@Test(expected = Invoker.MethodInvocationException.class)
-	public void testInvoke_FailedInvocationInvalidTypes() {
+	public void invokeFailsBecauseOfInvalidTypesOfArguments() {
 		parentAndChildInvoker.invoke(new MethodInvocation("f2", "a", "b"));
 	}
 
 	public static class ParentClass {
-
 		public static String f1() {
 			return "parent-f1";
 		}
@@ -78,7 +75,6 @@ public class InvokerTest {
 	}
 
 	public static class ChildClass extends ParentClass {
-
 		public static String f1() {
 			return "child-f1";
 		}
@@ -94,7 +90,5 @@ public class InvokerTest {
 		protected static String f12() {
 			return "f12";
 		}
-
 	}
-
 }

21ce69e7013b696031a2de31ed4be4d26ad346c8
Vytenis Bivainis <monrealis@gmail.com>
2017-04-06 00:05:20 +0300

    Restore JDK 1.5 support

--- skaitvardziai-parser/src/main/java/eu/vytenis/skaitvardziai/parser/methods/MethodKeys.java
+++ skaitvardziai-parser/src/main/java/eu/vytenis/skaitvardziai/parser/methods/MethodKeys.java
@@ -4,6 +4,6 @@ import java.lang.reflect.Method;
 
 public class MethodKeys {
 	public static MethodKey getKey(Method method) {
-		return new MethodKey(method.getName(), method.getParameterCount());
+		return new MethodKey(method.getName(), method.getParameterTypes().length);
 	}
 }

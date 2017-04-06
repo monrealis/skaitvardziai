2011898cf5d91afeadb0adb8d6cc45a98d225104
Vytenis Bivainis <monrealis@gmail.com>
2017-04-07 00:22:23 +0300

    Split one test method to many

--- skaitvardziai-app/src/test/java/eu/vytenis/skaitvardziai/app/template/TemplateProcessorTest.java
+++ skaitvardziai-app/src/test/java/eu/vytenis/skaitvardziai/app/template/TemplateProcessorTest.java
@@ -10,6 +10,8 @@ import eu.vytenis.skaitvardziai.app.AppTest;
 public class TemplateProcessorTest extends AppTest {
 	private String startTag;
 	private String endTag;
+	private String input;
+	private Object[] outputs;
 
 	@Before
 	public void before() {
@@ -18,58 +20,82 @@ public class TemplateProcessorTest extends AppTest {
 	}
 
 	@Test
-	public void testCollectFragments() {
-		String input;
-		Object[] output;
-		input = "a${}${'a'}b\nc";
-		output = new Object[] {ss("a"), fs(""), fs("'a'"), ss("b\nc")};
-		assertParsedFragments(input, output);
-		input = "${c}";
-		output = new Object[] {fs("c")};
-		assertParsedFragments(input, output);
-		input = "${}${b}${c}";
-		output = new Object[] {fs(""), fs("b"), fs("c")};
-		assertParsedFragments(input, output);
+	public void empty() {
 		input = "";
-		output = new Object[] {};
-		assertParsedFragments(input, output);
-		input = "a\nb";
-		output = new Object[] {ss("a\nb")};
-		assertParsedFragments(input, output);
-		input = "a${b}";
-		output = new Object[] {ss("a"), fs("b")};
-		assertParsedFragments(input, output);
-		input = "a${b}c";
-		output = new Object[] {ss("a"), fs("b"), ss("c")};
-		assertParsedFragments(input, output);
-		input = "${a}b";
-		output = new Object[] {fs("a"), ss("b")};
-		assertParsedFragments(input, output);
+		outputs = new Object[] {};
+		assertParsedFragments();
 	}
 
 	@Test
-	public void testCollectFragmentsUsingOtherTags() {
-		String input;
-		Object[] output;
+	public void method() {
+		input = "${c}";
+		outputs = new Object[] {method("c")};
+		assertParsedFragments();
+	}
+
+	@Test
+	public void methods() {
+		input = "${}${b}${c}";
+		outputs = new Object[] {method(""), method("b"), method("c")};
+		assertParsedFragments();
+	}
+
+	@Test
+	public void string() {
+		input = "a\nb";
+		outputs = new Object[] {string("a\nb")};
+		assertParsedFragments();
+	}
+
+	@Test
+	public void metohdLast() {
+		input = "a${b}";
+		outputs = new Object[] {string("a"), method("b")};
+		assertParsedFragments();
+	}
+
+	@Test
+	public void methodFirst() {
+		input = "${a}b";
+		outputs = new Object[] {method("a"), string("b")};
+		assertParsedFragments();
+	}
+
+	@Test
+	public void methodInTheMiddle() {
+		input = "a${b}c";
+		outputs = new Object[] {string("a"), method("b"), string("c")};
+		assertParsedFragments();
+	}
+
+	@Test
+	public void methodsInTheMiddle() {
+		input = "a${}${'a'}b\nc";
+		outputs = new Object[] {string("a"), method(""), method("'a'"), string("b\nc")};
+		assertParsedFragments();
+	}
+
+	@Test
+	public void nonDefaultTags() {
 		startTag = "<?";
 		endTag = "?>";
 		input = "+<? c ?>-";
-		output = new Object[] {ss("+"), fs(" c "), ss("-")};
-		assertParsedFragments(input, output);
+		outputs = new Object[] {string("+"), method(" c "), string("-")};
+		assertParsedFragments();
 	}
 
-	private void assertParsedFragments(String text, Object[] expected) {
-		TemplateProcessor p = new TemplateProcessor(startTag, endTag, text, systemFiles);
+	private void assertParsedFragments() {
+		TemplateProcessor p = new TemplateProcessor(startTag, endTag, input, systemFiles);
 		p.createPattern();
 		p.collectFragments();
-		assertArrayEquals(expected, p.getFragments().toArray());
+		assertArrayEquals(outputs, p.getFragments().toArray());
 	}
 
-	private StringSource ss(String text) {
+	private StringSource string(String text) {
 		return new StringSource(text);
 	}
 
-	private MethodInvocationSource fs(String unparsedText) {
+	private MethodInvocationSource method(String unparsedText) {
 		return new MethodInvocationSource(unparsedText);
 	}
 

81f3a2d6eeb7414c53c15483f9ebe06b493e9f9f
Vytenis Bivainis <monrealis@gmail.com>
2020-12-24 00:06:33 +0200

    WIP: add some grammar

--- skaitvardziai-parser/src/main/antlr4/eu/vytenis/skaitvardziai/parser/antlr4/function.g4
+++ skaitvardziai-parser/src/main/antlr4/eu/vytenis/skaitvardziai/parser/antlr4/function.g4
@@ -2,24 +2,25 @@ grammar function;
 
 methodInvocation: identifier '(' parameters? ')';
 
-parameters: parameter ( ',' parameter )* ;
+parameters: parameter (',' parameter)*;
 parameter: fractionLiteral | integerLiteral | STRING | nullLiteral;
 
-integerLiteral: INTEGER;
-fractionLiteral: INTEGER '/' INTEGER;
+fractionLiteral: integerLiteral '/' integerLiteral;
+integerLiteral: MINUS? INTEGER;
 
 STRING: SINGLE_QUOTED_STRING | DOUBLE_QUOTED_STRING;
 SINGLE_QUOTED_STRING:  '\'' (~["'"])* '\'';
 DOUBLE_QUOTED_STRING: '"' (~["'"])* '"';
 
 identifier: IDENTIFIER;
-IDENTIFIER: LETTER (LETTER | DIGIT)*;
-LETTER: [a-zA-Z];
-DIGIT: [0-9];
 
-INTEGER: [0-9]+;
+fragment LETTER: [a-zA-Z];
+fragment DIGIT: [0-9];
+
+MINUS: '-';
+INTEGER: DIGIT+;
 nullLiteral: NULL;
 NULL : 'null';
+IDENTIFIER: LETTER (LETTER | DIGIT)*;
 
 WS  :  [ \t\r\n]+ -> skip;
-
--- skaitvardziai-parser/src/main/java/eu/vytenis/skaitvardziai/parser/methods/antrl4/Antlr4Methods.java
+++ skaitvardziai-parser/src/main/java/eu/vytenis/skaitvardziai/parser/methods/antrl4/Antlr4Methods.java
@@ -2,31 +2,43 @@ package eu.vytenis.skaitvardziai.parser.methods.antrl4;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.math.BigInteger;
+import java.util.Stack;
 
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CodePointCharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import eu.vytenis.skaitvardziai.parser.antlr4.functionBaseListener;
 import eu.vytenis.skaitvardziai.parser.antlr4.functionLexer;
 import eu.vytenis.skaitvardziai.parser.antlr4.functionParser;
+import eu.vytenis.skaitvardziai.parser.antlr4.functionParser.FractionLiteralContext;
+import eu.vytenis.skaitvardziai.parser.antlr4.functionParser.IdentifierContext;
+import eu.vytenis.skaitvardziai.parser.antlr4.functionParser.IntegerLiteralContext;
 import eu.vytenis.skaitvardziai.parser.antlr4.functionParser.MethodInvocationContext;
+import eu.vytenis.skaitvardziai.parser.antlr4.functionParser.NullLiteralContext;
 import eu.vytenis.skaitvardziai.parser.methods.MethodInvocation;
 import eu.vytenis.skaitvardziai.parser.methods.Methods;
+import eu.vytenis.skaitvardziai.skaiciai.SveikasisSkaicius;
+import eu.vytenis.skaitvardziai.skaiciai.Trupmena;
 
 public class Antlr4Methods extends Methods {
 	@Override
 	public MethodInvocation getMethodInvocation(String methodInvocationText) {
 		CharStream s = toCharStream(methodInvocationText);
 		CommonTokenStream tokens = new CommonTokenStream(new functionLexer(s));
-		MethodInvocationContext methodInvocation = new functionParser(tokens).methodInvocation();
+		System.out.println();
+		new functionLexer(toCharStream(methodInvocationText)).getAllTokens()
+				.forEach(t -> System.out.println("*" + t.getText()));
+		functionParser parser = new functionParser(tokens);
+		// parser.setErrorHandler(new BailErrorStrategy());
+		MethodInvocationContext methodInvocation = parser.methodInvocation();
 		ParseTreeWalker walker = new ParseTreeWalker();
-		ParseTreeListener listener = new MethodInvocationBuilder();
+		MethodInvocationBuilder listener = new MethodInvocationBuilder();
 		walker.walk(listener, methodInvocation);
-		return null;
+		return listener.toMethodInvocation();
 	}
 
 	private CodePointCharStream toCharStream(String methodInvocationText) {
@@ -38,5 +50,43 @@ public class Antlr4Methods extends Methods {
 	}
 
 	private static class MethodInvocationBuilder extends functionBaseListener {
+		private String name;
+		private Stack<Object> parameters = new Stack<Object>();
+
+		@Override
+		public void exitIdentifier(IdentifierContext ctx) {
+			name = ctx.getText();
+		}
+
+		@Override
+		public void exitFractionLiteral(FractionLiteralContext ctx) {
+			BigInteger v = pop();
+			BigInteger s = pop();
+			addParameter(new Trupmena(s, v));
+		}
+
+		private BigInteger pop() {
+			return ((SveikasisSkaicius) parameters.pop()).getReiksme();
+		}
+
+		@Override
+		public void exitNullLiteral(NullLiteralContext ctx) {
+			addParameter(null);
+		}
+
+		@Override
+		public void exitIntegerLiteral(IntegerLiteralContext ctx) {
+			String sign = ctx.MINUS() != null ? "-" : "";
+			SveikasisSkaicius s = new SveikasisSkaicius(sign + ctx.INTEGER().getText());
+			addParameter(s);
+		}
+
+		private void addParameter(Object parameter) {
+			parameters.add(parameter);
+		}
+
+		private MethodInvocation toMethodInvocation() {
+			return new MethodInvocation(name, parameters.toArray());
+		}
 	}
 }
